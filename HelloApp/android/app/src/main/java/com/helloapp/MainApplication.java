2e39b038b73954c4082164d29e26df9cf04175de
pocandroid <62979933+pocandroid@users.noreply.github.com>
2020-04-10 12:57:29 +0530

    Update MainApplication.java

--- HelloApp/android/app/src/main/java/com/helloapp/MainApplication.java
+++ HelloApp/android/app/src/main/java/com/helloapp/MainApplication.java
@@ -2,7 +2,10 @@ package com.helloapp;
 
 import android.app.Application;
 import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
